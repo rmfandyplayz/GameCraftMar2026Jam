12538dab7c5c7bf83f0525411edbc960d306ed11
MysticTortoise <shadowknight7788@gmail.com>
2026-03-20 22:37:57 -0600

    idfk laser protecjile

---
+++ Assets/Scripts/Baby/LaserProjectile.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using UnityEngine;
+
+public class LaserProjectile : DamagingObject
+{
+    [SerializeField] private float MoveSpeed;
+    [NonSerialized] public Vector2 moveDir;
+    
+    
+    private void Update()
+    {
+        transform.position += (Vector3)(moveDir.normalized * MoveSpeed);
+    }
+}

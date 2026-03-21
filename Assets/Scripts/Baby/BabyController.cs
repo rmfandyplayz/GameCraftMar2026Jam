73fa92b78172128bed1c892c56f02a0660bcf5b6
ocfrey <ocfrey270@gmail.com>
2026-03-21 14:55:51 -0600

    projectile stuff working well

--- Assets/Scripts/Baby/BabyController.cs
+++ Assets/Scripts/Baby/BabyController.cs
@@ -1,11 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Baby
 {
     public class BabyController : MonoBehaviour
     {
-        private float timer;
+        private float moveTimer;
+        private float laserTimer;
         [SerializeField] private float chillTime = 5f;
+        [SerializeField] private float shootRate = 3f;
+        
+        [SerializeField] private GameObject laserPrefab;
+        [SerializeField] private GameObject shardPrefab;
         private BabyMovementManager movement;
         [SerializeField] private MovementNode currentNode;
         private MovementNode nextGoal;
@@ -24,10 +30,22 @@ namespace Baby
         {
             if (!movement.isMoving)
             {
-                timer += Time.deltaTime;
-                if (timer >= chillTime)
+                moveTimer += Time.deltaTime;
+                if (moveTimer >= chillTime)
                 {
                     movement.MoveTo(currentNode, nextGoal);
+
+                    if (currentNode.location == NodeLocation.PlayRoom)
+                    {
+                        for (int i = 0; i <= 360; i += 30)
+                        {
+                            Quaternion angle = Quaternion.Euler(0, 0, i);
+                            GameObject shard = Instantiate(shardPrefab, transform.position, angle);
+                            BlockShards proj = shard.GetComponent<BlockShards>();
+                            proj.moveDir = shard.transform.up;
+                        }
+                    }
+
                     movement.isMoving = true;
                     currentNode = nextGoal;
                     nextGoal = movement.goalNodes[Random.Range(0, movement.goalNodes.Count)];
@@ -35,7 +53,19 @@ namespace Baby
                     {
                         nextGoal = movement.goalNodes[Random.Range(0, movement.goalNodes.Count)];
                     }
-                    timer = 0;
+                    moveTimer = 0;
+                }
+            }
+            else
+            {
+                laserTimer +=  Time.deltaTime;
+                if (laserTimer >= shootRate)
+                {
+                    Quaternion angle = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+                    GameObject laser = Instantiate(laserPrefab, transform.position, angle);
+                    LaserProjectile proj = laser.GetComponent<LaserProjectile>();
+                    proj.moveDir = laser.transform.right;
+                    laserTimer = 0;
                 }
             }
         }

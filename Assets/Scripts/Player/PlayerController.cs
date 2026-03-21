70fe6cfddacbe25b4f37175b41d6183701164b9a
MysticTortoise <shadowknight7788@gmail.com>
2026-03-20 23:05:36 -0600

    player controller upgrades

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Player player;
+
+    private void Start()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
+
+    private void Update()
+    {
+        player.move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+}

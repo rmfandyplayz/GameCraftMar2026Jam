d2813f39bc0f5622928c69b797ce4395a84de68e
MysticTortoise <shadowknight7788@gmail.com>
2026-03-20 23:12:09 -0600

    baby mind controller

---
+++ Assets/Scripts/Baby/BabyMindController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BabyMindController : MonoBehaviour
+{
+    private Player player;
+    private float timer;
+    
+    private void Update()
+    {
+        if (!player)
+            player = FindAnyObjectByType<Player>();
+
+        timer -= Time.deltaTime;
+        if (!(timer <= 0)) return;
+        
+        timer = 0.5f;
+        int rand = Random.Range(0, 3);
+        player.move = rand switch
+        {
+            0 => Vector2.right,
+            1 => Vector2.down,
+            2 => Vector2.left,
+            3 => Vector2.up,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
